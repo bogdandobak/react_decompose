8eac38efd89cf5c7694687be8482d9559add176d
Bohdan Dobak <chudolipton@gmail.com>
2021-03-04 15:13:44 +0200

    Updates

---
+++ precache-manifest.c7d9c1a34e5f6e99993346cddd3870f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2129dd92133c6feacfb3e1e9153878d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "543b801e8eafb4f386e7",
+    "url": "./static/css/main.f6abfb1c.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543b801e8eafb4f386e7",
+    "url": "./static/js/main.ec21294b.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
